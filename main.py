04bf71959bad137745758c53e649aa5d72fed43f
Mira <mira@legendes-oubliees.org>
2024-11-25 09:54:54 +0100

    Warning about wrong commande

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ while commande != 'exit':
         b = input("quelle nage ? ")
         c = input("combien de longueur ? ")
         liste.append((a,b,c))
+        continue
    
     if commande == 'liste':
         for elt in liste:
             print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+        continue
+
+    print(f"Commande {commande} inconnue")
