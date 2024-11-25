1f6bdbc72b54111c0ede49a560298b4adb62ed76
Mira <mira@legendes-oubliees.org>
2024-11-25 09:36:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+liste = [("Pierre","Dos",10),("Paul","Brasse",13),("Léa","Crawl",6), ("Léa","Brasse",8) ]
+commande = ''
+
+while commande != 'exit':
+    commande = input("Que faut-il faire ? ")
+
+    if commande == 'ajout':
+        a = input("Qui nage ? ")
+        b = input("quelle nage ? ")
+        c = input("combien de longueur ? ")
+        liste.append((a,b,c))
+   
+    if commande == 'liste':
+        for elt in liste:
+            print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
