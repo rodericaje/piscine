bf56a2f248ca51f075cba3eed9df9ec00c2bed37
Mira <mira@legendes-oubliees.org>
2024-11-25 10:23:50 +0100

    Create exit command

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 liste = [("Pierre","Dos",10),("Paul","Brasse",13),("Léa","Crawl",6), ("Léa","Brasse",8) ]
 commande = ''
 
-while commande != 'exit':
+isAlive = True
+while isAlive:
     commande = input("Que faut-il faire ? ")
 
     if commande == 'ajout':
@@ -14,6 +15,12 @@ while commande != 'exit':
     if commande == 'liste':
         for elt in liste:
             print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+        continue
+
+    if commande == 'exit':
+        tmp = input("En êtes-vous sûr ? (o)ui/(n)on")
+        if tmp == 'o':
+            isAlive = False
         continue
 
     print(f"Commande {commande} inconnue")
