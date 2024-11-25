e936de57cccd0393a565d96d137c3dabaf435362
Mira <mira@legendes-oubliees.org>
2024-11-25 10:47:00 +0100

    Create functions

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 liste = [("Pierre","Dos",10),("Paul","Brasse",13),("Léa","Crawl",6), ("Léa","Brasse",8) ]
 commande = ''
 
+def cmd_ajout(liste):
+    """Ajoute un évènement à la liste"""
+    a = input("Qui nage ? ")
+    b = input("quelle nage ? ")
+    c = input("combien de longueur ? ")
+    liste.append((a,b,c))
+
+def cmd_liste(liste):
+    """Affiche toutes les performances des nageurs"""
+    for elt in liste:
+        print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+
+
 isAlive = True
 while isAlive:
     commande = input("Que faut-il faire ? ")
 
     if commande == 'ajout':
-        a = input("Qui nage ? ")
-        b = input("quelle nage ? ")
-        c = input("combien de longueur ? ")
-        liste.append((a,b,c))
+        cmd_ajout(liste)
         continue
    
     if commande == 'liste':
-        for elt in liste:
-            print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+        cmd_liste(liste)
         continue
 
     if commande == 'exit':
